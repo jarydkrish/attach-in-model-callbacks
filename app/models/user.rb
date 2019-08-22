acf39925ec39c7dac2f3eef4dddfbbbea6e7a8d5
Jaryd Krishnan <jaryd@harvestprofit.com>
2019-08-22 17:11:53 -0500

    Clarify example

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
 
   attr_accessor :copy_avatar_from_user_id
 
-  # Either of these methods will give the same result
+  # Callback that triggers an endless loop
   after_save :copy_avatar, if: :should_copy_avatar_from_user?
-  # after_save :copy_avatar_via_tempfile, if: :should_copy_avatar_from_user?
-
 
   def should_copy_avatar_from_user?
     copy_avatar_from_user_id.present?
@@ -16,17 +14,5 @@ class User < ApplicationRecord
   def copy_avatar
     user_to_copy_from = User.find_by(id: copy_avatar_from_user_id)
     avatar.attach(user_to_copy_from.avatar.blob)
-  end
-
-  # Copy the avatar via a tempfile
-  def copy_avatar_via_tempfile
-    user_to_copy_from = User.find_by(id: copy_avatar_from_user_id)
-    user_to_copy_from.avatar.blob.open do |tempfile|
-      avatar.attach(
-        io: tempfile,
-        filename: user_to_copy_from.avatar.filename,
-        content_type: user_to_copy_from.avatar.content_type
-      )
-    end
   end
 end
